bce5edf1aa4c55db5e3636d19507802c2ab3d342
ItinoseSan <jkekun@gmail.com>
2017-06-22 21:53:39 +0900

    Update Main.java

--- TwitterAPI/src/com/company/Main.java
+++ TwitterAPI/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main { //実装する理想
 
     public static void main(String[] args) {
-        TweetInterface twin = new TweetMessage("");
+        TweetInterface twin = new TweetMessage("Hello World!!");
         TwitterAPI twitter = new TwitterAPI("", "", "", "");
         twitter.Tweet(twin.sentence());
     }
