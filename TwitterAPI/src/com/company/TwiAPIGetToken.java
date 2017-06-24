d883725e003e134a4e0e023765c9b9c5a3ef01e8
ItinoseSan <jkekun@gmail.com>
2017-06-24 21:23:44 +0900

    Update TwiAPIGetToken.java

--- TwitterAPI/src/com/company/TwiAPIGetToken.java
+++ TwitterAPI/src/com/company/TwiAPIGetToken.java
@@ -10,24 +10,6 @@ public class TwiAPIGetToken
     private String ACCESSTOKEN;
     private String ACCESSSECRET;
 
-
-
-    byte[]signatureHash;
-
-    public TwiAPIGetToken(String CONSUMER_KEY, String CONSUMER_KEY_SECRET, String ACCESSSTOKEN, String ACCESSSECRET){
-        this.CONSUMER_KEY=CONSUMER_KEY;
-        this.CONSUMER_KEY_SECRET=CONSUMER_KEY_SECRET;
-        this.ACCESSTOKEN=ACCESSSTOKEN;
-        this.ACCESSSECRET=ACCESSSECRET;
-        try {
-            signatureHash = CONSUMER_KEY_SECRET.getBytes("US-ASCII");
-            signatureHash =ACCESSSECRET.getBytes("US-ASCII");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return;
-        }
-    }
     public String getCONSUMER_KEY(){
         return CONSUMER_KEY;
     }
