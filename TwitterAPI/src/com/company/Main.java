469e08135c792b97e1d7c9b8d80ddf2909b7cd48
ItinoseSan <jkekun@gmail.com>
2017-07-02 15:47:40 +0900

    Update Main.java

--- TwitterAPI/src/com/company/Main.java
+++ TwitterAPI/src/com/company/Main.java
@@ -8,6 +8,6 @@ public class Main { //実装する理想
     public static void main(String[] args) throws IOException {
         TwiJava updateJson = new TwiJava("ConsumerKey", "ConsumerSecret","AccessToken", "AccessTokenSecret");
         System.out.println(updateJson.tweet("Hello World"));
-        System.out.println(updateJson.gethomeTimeline());
+        System.out.println(updateJson.GetHomeTimeline());
     }
 }
