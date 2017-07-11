cb39f22c9908eb7d70af957ef452b1841d9df08a
ItinoseSan <jkekun@gmail.com>
2017-07-11 20:03:55 +0900

    Commited Gradle ver

---
+++ TwiJava/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import TwiJavaMainFunction.TwiJava;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        TwiJava twitter = new TwiJava("ConsumerKey", "ConsumerSecret","AccessToken", "AccessTokenSecret");
+       // System.out.println(twitter.tweet("Hello World"));
+        System.out.println(twitter.getHomeTimeLine("5"));
+        System.out.println(twitter.getUserTimeLine("5"));
+    }
+}
