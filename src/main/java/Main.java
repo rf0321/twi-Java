bf195804a8e6953ac2930605b698c80f95c11b4e
ItinoseSan <jkekun@gmail.com>
2017-09-17 22:13:15 +0900

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        TwiJava twitter = new TwiJava("39PLQU2FQhR8vaDxuGo8Y3v5P", "3bm3JaTusFtpwv1R8Yxxh4ko6OkCLeGKOcgbsNvQSnfumFBVBz",
-                "865875664074792960-2OYUu49otbCkov5yKH7K4FD4EbRYLEw", "YFUr5FJ9hzc0drAnIsb21pdz8CGel7WjKgYuExvp6K2hj");
+        TwiJava twitter = new TwiJava("ConsumerKey", "ConsumerSecret",
+                "AccessToken", "AccessTokenSecret");
 
         System.out.println(twitter.tweet("Hello World"));
         System.out.println(twitter.getHomeTimeLine("5"));
