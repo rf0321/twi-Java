506bb4c0ff337a9d6a6c32083c0a07704d2cfb62
ItinoseSan <jkekun@gmail.com>
2017-09-29 17:52:27 +0900

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,10 @@ import twijava.TwiJava;
 public class Main {
     public static void main(String[] args) throws Exception {
         TwiJava twitter=new TwiJava.TwiJavaToken()
-                .setConsumerKey("P0pYagYZV2DuEGgcxLTpREf62")
-                .setConsumerSecretKey("rmt8HuTJ5N01DXMK1NT2XgNS5eoUXOPU0DNBcHeh7xaBX5zTCy")
-                .setAccessToken("865875664074792960-2OYUu49otbCkov5yKH7K4FD4EbRYLEw")
-                .setAccessTokenSecret("YFUr5FJ9hzc0drAnIsb21pdz8CGel7WjKgYuExvp6K2hj")
+                .setConsumerKey("consumerkey")
+                .setConsumerSecretKey("consumerkeysecret");
+                .setAccessToken("accessToken");
+                .setAccessTokenSecret("accessTokensecret");
                 .buildTokens();
         System.out.println(twitter.tweet("おはよう"));
         System.out.println(twitter.getHomeTimeline(5));
