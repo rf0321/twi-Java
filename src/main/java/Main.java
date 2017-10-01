4a77949bebc4870ebbb79e0a3f4281564d6e1cae
ItinoseSan <jkekun@gmail.com>
2017-10-01 16:24:38 +0900

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import twijava.TwiJava;
 public class Main {
     public static void main(String[] args) throws Exception {
         TwiJava twitter = new TwiJava.SetAPIToken()
-                .setConsumerKey("P0pYagYZV2DuEGgcxLTpREf62")
-                .setConsumerSecretKey("rmt8HuTJ5N01DXMK1NT2XgNS5eoUXOPU0DNBcHeh7xaBX5zTCy")
-                .setAccessToken("865875664074792960-2OYUu49otbCkov5yKH7K4FD4EbRYLEw")
-                .setAccessTokenSecret("YFUr5FJ9hzc0drAnIsb21pdz8CGel7WjKgYuExvp6K2hj")
+                .setConsumerKey("ck")
+                .setConsumerSecretKey("cks")
+                .setAccessToken("at")
+                .setAccessTokenSecret("ats")
                 .buildTokens();
         twitter.getHomeTimeLine(100,false);
         twitter.getHomeTimeLine(100,true);
